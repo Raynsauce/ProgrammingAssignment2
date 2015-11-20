ed65e9b4431544920c73c5306de3fcdcdf4a11df
ShadowMassacr13 <markiixxoo@gmail.com>
2015-11-19 20:27:33 -0800

    Completed assignment
    Tested with a 2x2 and 3x3 invertible matrix, non-invertible matrices
    throw an error as intended.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+##Simply edited from the example given on Github for vectors;
 makeCacheMatrix <- function(x = matrix()) {
+        mtx <- NULL    #Init m, our matrix
+        set <- function(y)
+        {
+            x <<- y     #Generic variable names ftw
+            mtx <<- NULL
+        }
+        #Return the value of the current matrix
+        get <- function(){ return(x) }
+        setinvert <- function(inverse) mtx <<- inverse
+        getinvert <- function(){ return(mtx) }
 
+        return(list(set = set,get = get,
+                    setinvert = setinvert,
+                    getinvert = getinvert))
+        #I prefer explicit return() statements
 }
 
-
-## Write a short comment describing this function
-
+#Inverts the matrix after using makeCacheMatrix(),
+#the result is stored in cache after computation,
+#if it exists in cache, it's simply returned;
+#This function assumed the matrix is invertable:
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+    #Attempt to read in the cached matrix:
+    mtx <- x$getinvert()
+    if(!is.null(mtx)){  ##Check if the matrix exists
+        message("Getting cached data...")
+        return(mtx)
+    }
+
+    #No cached matrix exists; compute one:
+    mtxdat <- x$get() #make a temporary to pass to solve()
+    mtx <- qr.solve(mtxdat)
+    #qr.solve is simply a faster version of solve()
+    x$setinvert(mtx)  #This pushes m back into the cache
+
+    return(mtx)
+} #Newline at the end to make Github happy
